e955ca64c05836f9638d3ca961ed32a2dc1599b1
Jason Vann <vannjt1@icloud.com>
2021-10-12 22:03:57 -0400

    friendList

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Login from './components/login';
+import PrivateRoute from './components/PrivateRoute';
+import FriendsList from './components/FriendsList';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      Hello
+      <Link to='/logout'>Logout</Link>
+      <Link to='/friends' style={{ marginRight: '16px'}}>
+          See Friends
+        </Link>
+      <Link to='/friends/add-new'>Add Friend</Link>
+      <Switch>
+      {/* <PrivateRoute path='/friends/add-new' component={AddFriendForm} /> */}
+      <PrivateRoute path='/friends' component={FriendsList} />
+        <Route path="/login">
+          <Login/>
+        </Route>
+        <Route>
+            <Login />
+          </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
--- friends/src/components/login.js
+++ friends/src/components/login.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react'
+import axios from 'axios'
+import { useHistory } from 'react-router-dom'
 
+
+const initialValues = {
+    username: '',
+    password: ''
+  }
 
 function Login() {
+    const { push } = useHistory()
+    const [formValues, setFormValues] = useState(initialValues)
 
+    const handleChanges = (e) => {
+        setFormValues({...formValues, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        axios.post('http://localhost:5000/api/login', formValues)
+            .then(res=>{
+                window.localStorage.setItem('token', res.data.payload)
+                push('/friends')
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+    }
 
     return (
         <form onSubmit={handleSubmit}>
