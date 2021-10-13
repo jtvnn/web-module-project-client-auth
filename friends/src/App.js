a05ac479a31975f4efafb9127bc43921a490a787
Jason Vann <vannjt1@icloud.com>
2021-10-12 22:14:22 -0400

    addFriendForm

--- friends/src/App.js
+++ friends/src/App.js
@@ -4,18 +4,32 @@ import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Login from './components/login';
 import PrivateRoute from './components/PrivateRoute';
 import FriendsList from './components/FriendsList';
+import AddFriendForm from './components/addFriendForm';
 
 function App() {
+  
+
   return (
     <Router>
     <div className="App">
-      <Link to='/logout'>Logout</Link>
-      <Link to='/friends' style={{ marginRight: '16px'}}>
+      <ul>
+      <li>
+          <Link to='/login'>Login</Link>
+        </li>
+        <li>
+          <Link to='/logout'>Logout</Link>
+        </li>
+        <li>
+        <Link to='/friends' style={{ marginRight: '16px'}}>
           See Friends
         </Link>
-      <Link to='/friends/add-new'>Add Friend</Link>
+        </li>
+        <li>
+          <Link to='/friends/add-new'>Add Friend</Link>
+        </li>
+      </ul>
       <Switch>
-      {/* <PrivateRoute path='/friends/add-new' component={AddFriendForm} /> */}
+      <PrivateRoute path='/friends/add-new' component={AddFriendForm} />
       <PrivateRoute path='/friends' component={FriendsList} />
         <Route path="/login">
           <Login/>
